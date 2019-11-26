e4d848a61b339e225fc2048eeb53bd2a27ba0e6f
Canada <james-canada@mohawkcollege.ca>
2019-11-26 10:32:45 -0500

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    int score = 0;
+    public Text text;
+
+    
+
+    public void UpdateScore(int points)
+    {
+        score += points;   
+        text.text = ($"score: {score}");
+    }
+
+    /*private void OnEnable()
+    {
+        Player.OnScorePoints += UpdateScore;
+    }
+
+    private void OnDisable()
+    {
+        Player.OnScorePoints -= UpdateScore;
+    }*/
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class Player : MonoBehaviour
+{
+    private int score = 0;
+
+    /*public delegate void ScorePointsAction();
+    public static ScorePointsAction OnScorePoints;*/
+
+    public class PointsEvent : UnityEvent<int> { }
+    public PointsEvent OnScorePoints;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Medallion")
+        {
+            Destroy(other.gameObject);
+            OnScorePoints.Invoke(1);
+        }
+    }
+}
